f88b30d80f01869f4bad9e13ac9f45d1cfad5d33
anmoljauhar1 <149699784+anmoljauhar1@users.noreply.github.com>
2025-07-25 21:57:46 +0530

    'changes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { ScrollTrigger,SplitText } from 'gsap/all';
+import gsap from 'gsap';
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  )
+}
+
+export default App
